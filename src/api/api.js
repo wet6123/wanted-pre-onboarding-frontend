4f75332ab65a4c2fcf72488ef4a26b72874181d5
JunSeong <gentlmanjun@naver.com>
2023-04-09 15:17:38 +0900

    feat: todo api 추가

--- src/api/api.js
+++ src/api/api.js
@@ -5,6 +5,11 @@ const config = {
   "Content-Type": "application/json",
 };
 
+const authConfig = (access_token) => ({
+  Authorization: access_token === null ? "" : `Bearer ${access_token}`,
+  "Content-Type": "application/json",
+});
+
 export const signin = async (payload) => {
   try {
     const res = await axios.post(api.signin(), payload, config);
@@ -17,6 +22,46 @@ export const signin = async (payload) => {
 export const signup = async (payload) => {
   try {
     const res = await axios.post(api.signup(), payload, config);
+    return res;
+  } catch (err) {
+    return null;
+  }
+};
+
+export const createTodo = async (payload) => {
+  try {
+    const token = window.localStorage.getItem("access_token");
+    const res = await axios.post(api.createTodo(), payload, authConfig(token));
+    return res.data;
+  } catch (err) {
+    return null;
+  }
+};
+
+export const getTodos = async () => {
+  try {
+    const token = window.localStorage.getItem("access_token");
+    const res = await axios.get(api.getTodos(), authConfig(token));
+    return res.data;
+  } catch (err) {
+    return null;
+  }
+};
+
+export const updateTodo = async (id, payload) => {
+  try {
+    const token = window.localStorage.getItem("access_token");
+    const res = await axios.put(api.updateTodo(id), payload, authConfig(token));
+    return res.data;
+  } catch (err) {
+    return null;
+  }
+};
+
+export const deleteTodo = async (id, payload) => {
+  try {
+    const token = window.localStorage.getItem("access_token");
+    const res = await axios.delete(api.updateTodo(id), authConfig(token));
     return res;
   } catch (err) {
     return null;
--- src/api/constant.js
+++ src/api/constant.js
@@ -1,13 +1,19 @@
 const API_BASE_URL = "https://www.pre-onboarding-selection-task.shop";
 
 const AUTH_URL = "/auth";
+const TODO_URL = "/todos";
 
+// auth
 const SIGNUP_URL = "/signup";
 const SIGNIN_URL = "/signin";
 
 const api = {
   signup: () => API_BASE_URL + AUTH_URL + SIGNUP_URL,
   signin: () => API_BASE_URL + AUTH_URL + SIGNIN_URL,
+  createTodo: () => API_BASE_URL + TODO_URL,
+  getTodos: () => API_BASE_URL + TODO_URL,
+  updateTodo: (id) => API_BASE_URL + TODO_URL + `/${id}`,
+  deleteTodo: (id) => API_BASE_URL + TODO_URL + `/${id}`,
 };
 
 export default api;
